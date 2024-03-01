a61f6802acb09c5ed6e844aa11c8f6caf04fc8df
danylosus <danil.sushko19@gmail.com>
2024-03-01 17:12:23 +0200

    Completed

--- lib/content.dart
+++ lib/content.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 
+import "dart:math";
+
+const textStyle = TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16);
+
 class Content extends StatefulWidget {
-  const Content({super.key});
+
+  const Content({
+    super.key,
+  });
 
   @override
-  State<Content> createState() {
-    return _ContentState();
-  }
+  State<Content> createState() => _ContentState();
 }
 
-const textStyle = TextStyle(color: Colors.white, fontWeight: FontWeight.w500);
-
 class _ContentState extends State<Content> {
-  final List<String> _firstTexts = ["Danylo", "Empat"];
-  final List<String> _secondTexts = ["Sushko", "Tech"];
-
-  int step = 0;
+  Texts texts = Texts(["Danylo", "Empat", "#0056B9", "Я", "У мене"], ["Sushko", "Tech", "#FFD800", "Вася Пупкін", "все вийшло"]);
 
   void _updateStep() {
     setState(() {
-      step = (step + 1) % _firstTexts.length;
+      texts.step = Random().nextInt(texts.firstTexts.length);
     });
   }
 
@@ -28,22 +28,34 @@ class _ContentState extends State<Content> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 12,
           children: [
-            Text(_firstTexts[step], style: textStyle,),
-            const SizedBox(width: 12,),
-            Text(_secondTexts[step],style: textStyle,),
+            Text(texts.firstTexts[texts.step], style: textStyle),
+            Text(texts.secondTexts[texts.step], style: textStyle),
           ],
         ),
         TextButton(
           onPressed: _updateStep,
           child: const Text(
             "Button",
-            style: textStyle
+            style: textStyle,
           ),
         )
       ],
     );
   }
+}
+
+class Texts {
+  int _step = 0;
+
+  List<String> firstTexts = [""];
+  List<String> secondTexts = [""];
+
+  int get step => _step;
+  set step(newStep) => _step = newStep;
+
+  Texts(this.firstTexts, this.secondTexts);
 }
